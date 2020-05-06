268060574b5aad67cd9763acd35ec893b7e087d4
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-05-07 00:54:33 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -11,50 +11,30 @@ from sklearn import svm
 import optuna
 
 
-trainDf=pd.read_csv("train.csv")
+trainDf=pd.read_csv("traink.csv")
 testDf=pd.read_csv("test.csv")
 
-trainX=trainDf[["Pclass","Age","Fare"]]
-trainX=trainX.fillna(trainX.median())
+trainX=trainDf[["Pclass","Age","Sex","Fare","Embarked"]]
+trainX=pd.get_dummies(trainX.fillna(trainX.median()))
 trainY=trainDf["Survived"]
 
-testX=testDf[["Pclass","Age","Fare"]]
-testX=testX.fillna(testX.median())
+testX=testDf[["Pclass","Age","Sex","Fare","Embarked"]]
+testX=pd.get_dummies(testX.fillna(testX.median()))
 
-# clf = svm.SVC( kernel='rbf',C=100, gamma=0.001)
-# clf.fit(trainX,trainY)
-# result=cross_val_score(clf,trainX,trainY,cv=6)
+
 def objective(trial):
-    # C
-    C = trial.suggest_uniform('C', 10, 1000)
+    C = trial.suggest_uniform('C', 1, 10)
     gamma = trial.suggest_uniform('gamma', 1e-4, 1e-2)
-    clf = svm.SVC(kernel='rbf',C=C, gamma=gamma)
+    clf = svm.SVC(C=C, gamma=gamma)
     return -cross_validate(clf,trainX,trainY,cv=6)['test_score'].mean()
-
-#study = optuna.create_study(study_name="ex",storage='sqlite:///example.db', load_if_exists=True)
-#study.optimize(objective, n_trials=100)
-study = optuna.load_study(study_name="ex",storage='sqlite:///example.db')
+study = optuna.create_study(study_name="ex",storage='sqlite:///example.db', load_if_exists=True)
+study.optimize(objective, n_trials=100)
+#study = optuna.load_study(study_name="ex",storage='sqlite:///example.db')
 print("\n===\n")
 print(study.best_trial)
 
-
-clf = svm.SVC( kernel='rbf',C=study.best_params["C"], gamma=study.best_params["gamma"])
+clf = svm.SVC(C=study.best_params["C"], gamma=study.best_params["gamma"],verbose=True)
 clf.fit(trainX,trainY)
 testDf["Survived"]=clf.predict(testX)
 testDf[["PassengerId","Survived"]].to_csv("./submission.csv",index=False)
 
-#svm.SVC(kernel='rbf',C=study.C, gamma=gamma)
-#testDf["Survived"]=clf.predict(testX)
-
-
-#clf = svm.SVC( kernel='rbf',C=100, gamma=0.001)
-#clf.fit(trainX,trainY)
-#result=cross_validate(clf,trainX,trainY,cv=6)['test_score'].mean()
-
-#print('params:', study.best_params)
-#print(result)
-
-
-#testDf["Survived"]=clf.predict(testX)
-
-#testDf[["PassengerId","Survived"]].to_csv("./submission.csv",index=False)
