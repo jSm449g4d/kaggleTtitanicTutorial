9af83fc97f95940648710a27471c01eae37c5c06
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-05-07 06:34:12 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -11,11 +11,25 @@ from sklearn import svm
 import optuna
 
 
-trainDf=pd.read_csv("traink.csv")
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(os.path.dirname(os.path.join("./", __file__)))
+
+def outlierDf(df):
+    #q_low = df.quantile(0.05)
+    #q_hi  = df.quantile(0.95)
+    #df.median()
+    tDfn=(df-df.min())/(df.max()-df.min())
+    return tDfn
+
+trainDf=pd.read_csv("train.csv")[["Pclass","Age","Sex","Fare","Embarked","Survived"]].dropna()
+trainDf[["Age","Fare"]]=outlierDf(trainDf[["Age","Fare"]])
 testDf=pd.read_csv("test.csv")
+testDf[["Age","Fare"]]=outlierDf(testDf[["Age","Fare"]])
 
 trainX=trainDf[["Pclass","Age","Sex","Fare","Embarked"]]
-trainX=pd.get_dummies(trainX.fillna(trainX.median()))
+#trainX=pd.get_dummies(trainX.fillna(trainX.median()))
+trainX=pd.get_dummies(trainX)
 trainY=trainDf["Survived"]
 
 testX=testDf[["Pclass","Age","Sex","Fare","Embarked"]]
@@ -28,7 +42,7 @@ def objective(trial):
     clf = svm.SVC(C=C, gamma=gamma)
     return -cross_validate(clf,trainX,trainY,cv=6)['test_score'].mean()
 study = optuna.create_study(study_name="ex",storage='sqlite:///example.db', load_if_exists=True)
-study.optimize(objective, n_trials=100)
+study.optimize(objective, n_trials=100, n_jobs=4)
 #study = optuna.load_study(study_name="ex",storage='sqlite:///example.db')
 print("\n===\n")
 print(study.best_trial)
